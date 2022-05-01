2a8ceaa0228436849685c65fa0cb68c91a05d648
Red <me@red.to>
2022-05-01 20:15:31 +0100

    Documentation

--- NanoleafTests/Sources/Tests/NanoleafClientTests.swift
+++ NanoleafTests/Sources/Tests/NanoleafClientTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import Nanoleaf
 
-final class NanoleafTests: XCTestCase {
+final class NanoleafClientTests: XCTestCase {
     private var apiClient: NanoleafClient!
     private var session: MockHTTPSession!
     
